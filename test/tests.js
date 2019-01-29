a40a9e88f85535974a5a3db08bc3da97b8dc56f7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-29 13:43:13 -0800

    added Buzz test and passing

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,19 @@ const test = QUnit.test;
 function fizzBuzz(number) {
     if(number % 3 === 0) {
         return 'Fizz';
+    } else if(number % 5 === 0) {
+        return 'Buzz';
     }
 }
 test('returns Fizz if number is divisible by 3', function(assert) {
     const inputNumber = 3;
     const result = fizzBuzz(inputNumber);
     const expected = 'Fizz';
+    assert.equal(result, expected);
+});
+test('returns Buzz if number is divisible by 5', function(assert) {
+    const inputNumber = 5;
+    const result = fizzBuzz(inputNumber);
+    const expected = 'Buzz';
     assert.equal(result, expected);
 });
