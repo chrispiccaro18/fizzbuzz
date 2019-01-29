37ca431103ac088c00cc85098d643c6ea5b05a0f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-29 13:37:18 -0800

    wrote first test

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,13 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function fizzBuzz(number) {
+    if(number % 3 === 0) {
+        return 'Fizz';
+    }
+}
+test('returns Fizz if number is divisible by 3', function(assert) {
+    const inputNumber = 3;
+    const result = fizzBuzz(inputNumber);
+    const expected = 'Fizz';
+    assert.equal(result, expected);
 });
