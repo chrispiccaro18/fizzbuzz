3b82ea52b9900353aecc1c62bd67faed14271090
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-29 15:06:15 -0800

    fizzbuzz test and passed

--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,38 @@
 const test = QUnit.test;
 
 function fizzBuzz(number) {
-    if(number % 3 === 0) {
-       return 'Fizz ' + number; 
+    if(number % 15 === 0) {
+        return 'FizzBuzz ' + number;
+    } else if(number % 3 === 0) {
+        return 'Fizz ' + number; 
     }
 }
+
+test('return FizzBuzz + number if number is divisible by 15', function(assert) {
+    const inputNumber = 15;
+    const result = fizzBuzz(inputNumber);
+    const expected = 'FizzBuzz 15';
+    assert.equal(result, expected);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 test('return Fizz + number when the number is divisible by 3', function(assert){
